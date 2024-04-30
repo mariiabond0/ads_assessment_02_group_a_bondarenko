dc08bed95190a60c78d514a4cd69a8d7c6aa5472
mariiabond0 <mariiabondarenko@MacBook-Air-Maria.local>
2024-04-30 11:11:49 +0200

    assignment 2 bondarenko

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 """
 Simple Calculator
 """
-# Provide your solution here
+def calculate(n1:int, n2:int, operator:str) -> str:
+    if n2 == 0:
+        print("Division by 0 is not allowed.")
+    else:
+        if operator == "+":
+            print(n1+n2)
+        elif operator == "-":
+            print(n1-n2)
+        elif operator == "*":
+            print(n1*n2)
+        elif operator == "/":
+            print(n1/n2)
+        else:
+            print("Invalid operator.")
 
+
+calculate(3,4, "+")
+calculate(2,8, "-")
+calculate(5,5, "*")
+calculate(10,0, "/")
+calculate(3,5, "/")
+calculate(9,4, "?")
 
 """
 Reverse Word
 """
-# Provide your solution here
+def reverse_word(word:str) -> str:
+    index = len(word) - 1
+    while index >= 0:
+        print(word[index])
+        index -= 1
+
+
+reverse_word("hello")
+reverse_word("WORLD")
+reverse_word("lEOn")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
